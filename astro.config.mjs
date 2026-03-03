02b126a80e002615bd54109fbd30e76c3d4a6a73
MrP01 <peter@waldert.at>
2026-03-03 18:21:18 +0100

    new domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://hereditary-test.waldert.at",
+  site: "https://demos.hereditary-project.eu",
   base: "/",
   integrations: [
     starlight({
