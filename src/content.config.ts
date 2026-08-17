88961429de8255a8ca3356f92f4e897f5f8c2722
MrP01 <peter@waldert.at>
2026-08-17 14:54:05 +0200

    reformat

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,25 +1,25 @@
 import { defineCollection } from "astro:content";
 import { docsLoader } from "@astrojs/starlight/loaders";
 import { docsSchema } from "@astrojs/starlight/schema";
-import { z } from 'astro/zod';
+import { z } from "astro/zod";
 import { glob } from "astro/loaders";
 
 const systemsCollection = defineCollection({
-  schema: ({ image }) => z.object({
-    title: z.string(),
-    image: image().optional(),
-    image_url: z.string().optional(),
-    link: z.string().optional(),
-    code: z.string().optional(),
-    video: z.string().optional(),
-    contact: z.string().optional(),
-    description: z.string().optional(),
-    paper: z.string().optional(),
-    documentation: z.string().optional(),
-    paper_title: z.string().optional(),
-
-  }),
-  loader: glob({ base: './src/content/systems', pattern: '**/*.{md,mdx}' }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      image: image().optional(),
+      image_url: z.string().optional(),
+      link: z.string().optional(),
+      code: z.string().optional(),
+      video: z.string().optional(),
+      contact: z.string().optional(),
+      description: z.string().optional(),
+      paper: z.string().optional(),
+      documentation: z.string().optional(),
+      paper_title: z.string().optional(),
+    }),
+  loader: glob({ base: "./src/content/systems", pattern: "**/*.{md,mdx}" }),
 });
 
 export const collections = {
